b0fe0eef78d7a3a59b96da3c6d994fa7f6e52448
Oleg <Olegmerkulov91@mail.ru>
2020-03-27 22:11:10 +0300

    ex 11

---
+++ src/java/Ex11.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Ex11 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String value = scanner.next();
+
+        System.out.println(value);
+    }
+}
