9a5f5af8b28c9e132f4c1e4e23abd7cb18980b4d
Oleg <Olegmerkulov91@mail.ru>
2020-03-27 22:13:02 +0300

    ex 13

---
+++ src/java/Ex13.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Ex13 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String firstValue = scanner.next();
+        String secondValue = scanner.next();
+
+        if (firstValue.length() > secondValue.length()) {
+            System.out.println("первая строка длиннее");
+        } else if (secondValue.length() > firstValue.length()) {
+            System.out.println("вторая строка длиннее");
+        } else {
+            System.out.println("строки равны по длине");
+        }
+    }
+}
