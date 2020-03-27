09db873dfe03c75299c633bdd5c884283b9f364a
Oleg <Olegmerkulov91@mail.ru>
2020-03-27 22:12:20 +0300

    ex 12

---
+++ src/java/Ex12.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Ex12 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String value = scanner.next();
+
+        System.out.println(value.trim());
+    }
+}
